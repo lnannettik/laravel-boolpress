e862de20473389f2f36ede44817cb956ef61918e
lnannettik <l.nannetti.k@gmail.com>
2022-02-03 19:07:17 +0100

    finito

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
     // Pagina CATEGORIA
 
     public function show($id) {
-        return 'Category Page';
+        $category = Category::find($id);
+
+        return view('admin.categories.show', compact('category'));
     }
 }
